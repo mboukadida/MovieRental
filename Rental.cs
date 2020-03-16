40ce7a41edaa976069a71c71fe9e167bce41e639
mboukadida <majed.boukadida@gmail.com>
2020-03-16 12:44:05 +0100

    First Upload

---
+++ Rental.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRental
+{
+    public class Rental
+    {
+        public Rental(int daysRented, Movie movie)
+        {
+            DaysRented = daysRented;
+            Movie = movie;
+        }
+
+        public Movie Movie { get; }
+        public int DaysRented { get; }
+    }
+}
